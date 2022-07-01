bd5a07250d6926423ce975276ee17230a511aa25
Peter Akerele <akerelepeter2020@gmail.com>
2022-07-01 15:14:29 -0700

    task four and five

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,3 +5,14 @@
   * Return: 0
 **/
 int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,3 +4,8 @@
  *description - Prints the numbers from 0-9.
  *Return: 0 on success
  */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
