289ac2450d81c57330ea700b6a274eb2a46650c3
Peter Akerele <akerelepeter2020@gmail.com>
2022-07-01 14:11:35 -0700

    task two to five

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main- Entry Point
- *
- * Return: Always 0 (success)
- *
- */
+*main - Printthe alphabetic
+* a program that prints the alphabet in lowercase, followed by a new line.
+*Return: Always (success)
+*/
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if  n > 5)
-	{
-	printf("Last digit of %d is greater than 5", n);
-	}
-	 if(n == 0)
-	{
-	printf("Last digit of %d is 0", n);
-	}
-	 if(n < 6 && n != 0)
-	{
-	printf("Last digit of %d and is less than 6", n);
-	}
-	return (0);
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *description - Prints the numbers from 0-9.
+ *Return: 0 on success
+ */
